ad5db2cd7a0f41a1757d1b6ae9d8b635f3857e4f
UP938212 <72561290+UP938212@users.noreply.github.com>
2024-03-24 22:03:23 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,6 @@ export default function Home() {
         
         </div>
         <div>
-       
        <a href="mailto:hamzaberqia.com" className="text-md flex justify-center  py-10">
        <span className='text-md text-gray-800 dark:text-white'>contact</span>
        <span className="text-md text-cyan-400">@hamzaberqia.com</span>
